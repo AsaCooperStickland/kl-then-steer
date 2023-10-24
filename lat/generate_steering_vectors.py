99a01fa4484dc8016b52136aab35e95c3ff15618
Asa Leonard Cooper Stickland <asacoopstick@gmail.com>
2023-10-24 18:45:55 -0400

    add attention perturbation, more data

--- lat/generate_steering_vectors.py
+++ lat/generate_steering_vectors.py
@@ -27,24 +27,49 @@ def generate_and_save_steering_vectors(
         model.get_logits(p_tokens)
         for layer in layers:
             p_activations = model.get_last_activations(layer)
+            p_activations_value = model.get_last_value_activations(layer)
+            p_activations_query = model.get_last_query_activations(layer)
             p_activations = p_activations[0, token_idx, :].detach().cpu()
-            positive_activations[layer].append(p_activations)
+            p_activations_value = p_activations_value[0, token_idx, :].detach().cpu()
+            p_activations_query = p_activations_query[0, token_idx, :].detach().cpu()
+            positive_activations[layer].append((p_activations, p_activations_value, p_activations_query))
         model.reset_all()
         model.get_logits(n_tokens)
         for layer in layers:
             n_activations = model.get_last_activations(layer)
+            n_activations_value = model.get_last_value_activations(layer)
+            n_activations_query = model.get_last_query_activations(layer)
             n_activations = n_activations[0, token_idx, :].detach().cpu()
-            negative_activations[layer].append(n_activations)
+            n_activations_value = n_activations_value[0, token_idx, :].detach().cpu()
+            n_activations_query = n_activations_query[0, token_idx, :].detach().cpu()
+            negative_activations[layer].append((n_activations, n_activations_value, n_activations_query))
     for layer in layers:
-        positive = torch.stack(positive_activations[layer])
-        negative = torch.stack(negative_activations[layer])
+        positive_activations_layer = [x[0] for x in positive_activations[layer]]
+        positive = torch.stack(positive_activations_layer)
+        negative_activations_layer = [x[0] for x in negative_activations[layer]]
+        negative = torch.stack(negative_activations_layer)
         vec = (positive - negative).mean(dim=0)
-        torch.save(vec, f"{output_folder}/vec_layer_{layer}.pt")
-        torch.save(positive, f"{output_folder}/positive_layer_{layer}.pt")
-        torch.save(
-            negative,
-            f"{output_folder}/negative_layer_{layer}.pt",
-        )
+        positive_activations_value_layer = [x[1] for x in positive_activations[layer]]
+        positive_value = torch.stack(positive_activations_value_layer)
+        negative_activations_value_layer = [x[1] for x in negative_activations[layer]]
+        negative_value = torch.stack(negative_activations_value_layer)
+        vec_value = (positive_value - negative_value).mean(dim=0)
+        positive_activations_query_layer = [x[2] for x in positive_activations[layer]]
+        positive_query = torch.stack(positive_activations_query_layer)
+        negative_activations_query_layer = [x[2] for x in negative_activations[layer]]
+        negative_query = torch.stack(negative_activations_query_layer)
+        vec_query = (positive_query - negative_query).mean(dim=0)
+        for activation_type, vectors in zip(["" , "_value", "_query"], 
+                                            [(vec, positive, negative), 
+                                             (vec_value, positive_value, negative_value), 
+                                             (vec_query, positive_query, negative_query)]):
+            vec_, positive_, negative_ = vectors
+            torch.save(vec_, f"{output_folder}/vec{activation_type}_layer_{layer}.pt")
+            torch.save(positive_, f"{output_folder}/positive{activation_type}_layer_{layer}.pt")
+            torch.save(
+                negative_,
+                f"{output_folder}/negative{activation_type}_layer_{layer}.pt",
+            )
 
 
 def generate_single_steering_vector(
