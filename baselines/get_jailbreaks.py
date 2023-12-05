fc93730289c68c5288b08986bafc4ebfc0761c6d
Jacob Pfau <jp6263@log-3.hpc.nyu.edu>
2023-12-05 16:45:13 -0500

    argparse clean up of get_jailbreaks.py

--- baselines/get_jailbreaks.py
+++ baselines/get_jailbreaks.py
@@ -1,54 +1,58 @@
+import argparse
 from lat import utils
 from baselines import autoDAN
-
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import json
 
-llama = AutoModelForCausalLM.from_pretrained('/vast/work/public/ml-datasets/llama-2/Llama-2-7b-chat-hf/', cache_dir='/scratch/jp6263/slackV2/hf/models/', torch_dtype=torch.float16).to('cuda')
-llama_tokenizer = AutoTokenizer.from_pretrained('/vast/work/public/ml-datasets/llama-2/Llama-2-7b-chat-hf/')
+def main(args):
+    # Load model and tokenizer
+    llama = AutoModelForCausalLM.from_pretrained(args.model_path, cache_dir=args.cache_dir, torch_dtype=torch.float16).to('cuda')
+    llama_tokenizer = AutoTokenizer.from_pretrained(args.model_path)
 
-#Get questions
-directory = './datasets/refusal/'
+    # Get questions
+    questions = []
+    with open(args.question_file, 'r') as file:
+        for line in file:
+            data = json.loads(line)
+            if any(category in data['category'] for category in args.categories):
+                questions.append(data)
 
-questions = []
-# Read the file and extract the question from the first line
-with open(directory+'questions.jsonl', 'r') as file:
-    for line in file:
-        data = json.loads(line)
-        if 'bias' in data['category'] or 'illegal' in data['category']:
-            questions.append(data)
+    formatted_system_prompt = f"<s>[INST] <<SYS>>\n{utils.system_prompt}\n<</SYS>>\n\n"
+    jailbreaks = []
 
+    for data in questions:
+        question = data['question']
+        connecting_string = question.strip() + ' ' + '[/INST]' + ' '
+        for i in range(args.BON):
+            initial_input = llama_tokenizer.decode(torch.randint(0, len(llama_tokenizer), (1, args.num_tokens)).to('cuda')[0])
+            gcg = autoDAN.PromptOptimizer(llama, llama_tokenizer, n_epochs=args.n_epochs, n_proposals=args.n_proposals, verbose=False)
+            out = gcg.optimize(formatted_system_prompt, initial_input, connecting_string, args.target_string)
+            print(out)
+            if out[2] < args.early_stop: 
+                new_entry = data.copy()
+                new_entry['jailbreak'] = out[1]
+                jailbreaks.append(new_entry)
+                break
 
-# GCG Parameters
-num_tokens = 15 #20 is default, using 15 as a sort of simplicity regularizer to avoid the question being changed during GCG optimization.
-n_proposals = 64 #Memory limited for rtx 8000
-n_epochs = 250 #Default is 500, using 250 with best-of-2 instead seems to work better.
+    with open(args.output_file, 'w') as outfile:
+        for entry in jailbreaks:
+            json.dump(entry, outfile)
+            outfile.write('\n')
 
-formatted_system_prompt = f"<s>[INST] <<SYS>>\n{utils.system_prompt}\n<</SYS>>\n\n"
-target_string = 'Sure, here'
-jailbreaks = []
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Script for optimizing prompts with GCG")
+    parser.add_argument('--model_path', type=str, default='/vast/work/public/ml-datasets/llama-2/Llama-2-7b-chat-hf/', help='Path to the model')
+    parser.add_argument('--cache_dir', type=str, default='/scratch/jp6263/slackV2/hf/models/', help='Cache directory for the model')
+    parser.add_argument('--question_file', type=str, default='./datasets/refusal/questions.jsonl', help='Path to the question file')
+    parser.add_argument('--output_file', type=str, default='./datasets/refusal/jailbreak_questions.jsonl', help='Path for the output file')
+    parser.add_argument('--num_tokens', type=int, default=15, help='Number of tokens for GCG')
+    parser.add_argument('--n_proposals', type=int, default=64, help='Number of proposals for GCG')
+    parser.add_argument('--n_epochs', type=int, default=250, help='Number of epochs for GCG')
+    parser.add_argument('--BON', type=int, default=2, help='Number of optimization iterations')
+    parser.add_argument('--categories', nargs='+', default=['bias', 'illegal'], help='List of categories to filter questions')
+    parser.add_argument('--target_string', type=str, default='Sure, here', help='Target string for optimization')
+    parser.add_argument('--early_stop', type=float, default=0.05, help='Early stopping threshol for GCG')
 
-for data in questions[:]:
-    question = data['question']
-    # formatted_system_prompt = f"<s>[INST] <<SYS>>\n{utils.system_prompt}\n<</SYS>>\n\n"+question.strip()
-    # connecting_string = ' ' + '[/INST]' + ' '
-    connecting_string = question.strip()+ ' ' + '[/INST]' + ' '
-    for i in range(2):
-        initial_input = llama_tokenizer.decode(torch.randint(0, len(llama_tokenizer), (1, num_tokens)).to('cuda')[0])
-        gcg = autoDAN.PromptOptimizer(llama, llama_tokenizer, n_epochs=n_epochs, n_proposals=n_proposals, verbose=False,)
-        out = gcg.optimize(formatted_system_prompt, initial_input, connecting_string, target_string,)
-        print(out)
-        if out[2]<0.05: 
-            final_tokens = llama_tokenizer.encode(out[0], return_tensors='pt', add_special_tokens=False)
-            sample = llama.generate(final_tokens.cuda(), max_length=len(final_tokens)+250, do_sample=True, temperature=1.)
-            print(llama_tokenizer.decode(sample[0][len(final_tokens):]))
-            new_entry = data.copy()
-            new_entry['jailbreak'] = out[1]
-            jailbreaks.append(new_entry)
-            break
-
-with open(directory + 'jailbreak_questions.jsonl', 'w') as outfile:
-    for entry in jailbreaks:
-        json.dump(entry, outfile)
-        outfile.write('\n')
+    args = parser.parse_args()
+    main(args)
