29502afbef3414e0b02626ce4eb6e2bd7e2f95bc
Asa Cooper Stickland <asacoopstick@gmail.com>
2023-12-30 17:01:13 +0000

    create new training data

--- scripts/format_training_data.py
+++ scripts/format_training_data.py
@@ -1,13 +1,13 @@
-
 import json
 
-# Replace 'input_file_path' with the path to your original JSON file
-input_file_path = "datasets/refusal/training/llama2_chat_7b/generated_training_0/vanilla_results_temp.json"
-output_file_path = "lat/finetuning/finetuning_data/training_0.json"
 
-def transform_data(input_path, output_path):
-    with open(input_path, 'r') as file:
-        data = json.load(file)
+def transform_data(input_paths, output_path):
+    data = []
+
+    for input_path in input_paths:
+        with open(input_path, 'r') as file:
+            data_subset = json.load(file)
+        data.extend(data_subset)
 
     transformed_data = []
 
@@ -24,5 +24,11 @@ def transform_data(input_path, output_path):
     with open(output_path, 'w') as file:
         json.dump(transformed_data, file, indent=4)
 
-# Call the function
-transform_data(input_file_path, output_file_path)
+
+if __name__ == "__main__":
+    # Call the function
+    input_file_paths = [
+        f"datasets/refusal/training/llama2_chat_7b/generated_training_{i}/vanilla_results_temp.json" for i in range(7)]
+    print(input_file_paths)
+    output_file_path = "lat/finetuning/finetuning_data/training_0.json"
+    transform_data(input_file_paths, output_file_path)
