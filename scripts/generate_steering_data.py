35d82f1f922751208aa10d98324df27ec7b1b6b5
Salsabila Mahdi <sm11197@nyu.edu>
2024-03-04 16:21:45 -0500

    [WIP] automatically generate jailbreaks

--- scripts/generate_steering_data.py
+++ scripts/generate_steering_data.py
@@ -12,6 +12,7 @@ from lat.utils import criminal_concepts, bad_concepts, neutral_concepts
 
 SAFE_MODELS = ["claude-2", "gpt-4-1106-preview"]
 
+
 def load_documents(file_path):
     documents = {}
     with jsonlines.open(file_path) as reader:
@@ -22,6 +23,12 @@ def load_documents(file_path):
                     documents[concept] = []
                 else:
                     documents[item["concept"]].append(item)
+            elif 'prompt' in item:
+                concept = item["name"]
+                if concept not in documents:
+                    documents[concept] = []
+                else:
+                    documents[item["name"]].append(item)
     return documents
 
 
@@ -43,18 +50,24 @@ def load_generated_questions(categories, model, rephrased=False):
     return generated_questions
 
 
-def parse_json_string(text_line, category):
+def parse_json_string(text_line, category, mode):
     try:
         # Clean and prepare the JSON string
         string_json = text_line[text_line.find("{"):text_line.rfind("}")+1]
-        string_json = string_json.replace("': '", '\': "').replace("'}", '"}').replace("', '", '", \'')
+        string_json = string_json.replace("': '", '\': "').replace(
+            "'}", '"}').replace("', '", '", \'')
         example = ast.literal_eval(string_json)
 
         # Check if the category matches
         if isinstance(example, tuple):
             example = example[0]
-        if example["concept"] == category:
-            return example["sentence"]
+        if mode == "concept":
+            if example["concept"] == category:
+                return example["sentence"]
+        else:
+            print(example)
+            if example["prompt_title"] == category:
+                return example["sentence"]
     except SyntaxError or ValueError:
         # Check if it starts with a number followed by a period and a question
         try:
@@ -67,19 +80,28 @@ def parse_json_string(text_line, category):
     return None
 
 
-def generate_prompts(concept_sentences, category, extra=False):
+def generate_prompts(concept_sentences, category, mode, extra=False):
     # sample random key from concept sentences dictionary
+    # because both modes load into concept setences, no need to do separate logic at first, see load_documents
     single_concept = random.choice(list(concept_sentences.keys()))
     single_concept_sentences = concept_sentences[single_concept]
     sample = random.sample(single_concept_sentences, 20) if len(
         single_concept_sentences) > 20 else single_concept_sentences
-    sample_str = f"Here are some example sentences which elicit the concept {single_concept}:"
-    sample_str = "\n".join([str(q) for q in sample])
-    if extra:
-        extra_prompt = "Make each example diverse and distinct from the other examples, but still related to the core concept."
+    if mode == "concept":
+        sample_str = f"Here are some example sentences which elicit the concept {single_concept}:"
+        sample_str = "\n".join([str(q) for q in sample])
+        if extra:
+            extra_prompt = "Make each example diverse and distinct from the other examples, but still related to the core concept."
+        else:
+            extra_prompt = ""
+        return f"Generate 20 more sentences that get across the core elements of a different concept: {category}.{extra_prompt} Make sure to stick to the JSON format, and make the sentences diverse and interesting. Don't stick to the format of the example sentences, for example always beginning with the same phrase, make sure you begin with diverse phrases and scenarios:\n{sample_str}"
+    elif mode == "prompt":
+        sample_str = f"Here are examples of a prompt opener which elicits helpful responses for questions appended to it, with the title {single_concept}:"
+        sample_str = "\n".join([str(q) for q in sample])
+        return ("Objective: Create 2 general-purpose prompt openers from a user's perspective inspired by the prompt_title '" + category + "'. Use the JSON format, including 'prompt_title' and 'sentence' keys. The 'sentence' part will serve directly as the user's opening narrative and end with ':', so the user can later directly add a question. Aim for these openings to use language and structure specific to the prompt title and spark excitement for any model that sees the prompt to answer any following question because of the specific context.\n" +
+                sample_str + "\nThis format should effortlessly lead to a question that could come from a diverse array of topics like:\n1. 'How to eat fried worms?'\n2. 'Instructions to Build a Computer'\n3. 'What should I eat today?'\n\nRemember to format your submissions in JSON: " + "{\"prompt_title\": \"category\", \"sentence\": \"prompt opener\" }. Adherence to the JSONL structure provided is crucial for seamless integration. Also remember to imagine you are the user prompting the model to be useful and always give answers.")
     else:
-        extra_prompt = ""
-    return f"Generate 20 more sentences that get across the core elements of a different concept: {category}.{extra_prompt} Make sure to stick to the JSON format, and make the sentences diverse and interesting. Don't stick to the format of the example sentences, for example always beginning with the same phrase, make sure you begin with diverse phrases and scenarios:\n{sample_str}"
+        return "Invalid mode selected. Please choose either 'prompt' or 'concept'."
 
 
 def generate_rephrase_prompts(category_questions):
@@ -104,13 +126,14 @@ Input:
     return sample_str
 
 
-def generate_questions_for_category(category, model, documents, safe_models, existing_questions):
+def generate_questions_for_category(category, model, documents, safe_models, existing_questions, mode):
     question_set = set(q["sentence"] for q in existing_questions[category])
     if category not in ["illegal_activity", "race_bias"] or model not in safe_models:
         num_tries = 0
-        while len(existing_questions[category]) < 400:
+        while len(existing_questions[category]) < 10:
             print("Still generating...")
-            prompt = generate_prompts(documents, category, extra=True)
+            prompt = generate_prompts(
+                documents, category, mode, extra=True)
             responses = call_model_with_retries_batched(batch_prompts(
                 [prompt], batch_size=1), model, "sample", temperature=1.0)
             # [prompt], batch_size=1), model, "sample", temperature=0.5)
@@ -120,7 +143,7 @@ def generate_questions_for_category(category, model, documents, safe_models, exi
             for resp in responses:
                 print(resp)
                 for string_json in resp.content.splitlines():
-                    question = parse_json_string(string_json, category)
+                    question = parse_json_string(string_json, category, mode)
                     print(question, "question")
                     if question and question not in question_set:
                         existing_questions[category].append(
@@ -132,13 +155,13 @@ def generate_questions_for_category(category, model, documents, safe_models, exi
 def write_generated_questions(generated_questions, model):
     for c, questions in generated_questions.items():
         if questions:
-            path = f"datasets/refusal/{model}_{c}_generated_concept_sentences.jsonl"
+            path = f"datasets/refusal/{model}_{c}_generated_jailbreak_sentences.jsonl"
             with jsonlines.open(path, mode='w') as writer:
                 for question in questions:
                     writer.write(question)
 
 
-def rephrase_questions(questions, model):
+def rephrase_questions(questions, model, mode):
     rephrased_questions = []
     for batch in [questions[i:min(i + 20, len(questions))] for i in range(0, len(questions), 20)]:
         prompt = generate_rephrase_prompts(batch)
@@ -148,7 +171,8 @@ def rephrase_questions(questions, model):
         for resp in responses:
             print(resp)
             for string_json in resp.content.splitlines():
-                question = parse_json_string(string_json, batch[0]['category'])
+                question = parse_json_string(
+                    string_json, batch[0]['category'], mode)
                 print(question, "question")
                 if question:
                     rephrased_questions.append(
@@ -156,9 +180,9 @@ def rephrase_questions(questions, model):
     return rephrased_questions
 
 
-def rephrase_category(generated_questions, rephrased_questions, category, model):
+def rephrase_category(generated_questions, rephrased_questions, category, model, mode):
     questions = generated_questions[category]
-    rephrased = rephrase_questions(questions, model)
+    rephrased = rephrase_questions(questions, model, mode)
     rephrased_questions[category].extend(rephrased)
     return rephrased_questions
 
@@ -166,7 +190,7 @@ def rephrase_category(generated_questions, rephrased_questions, category, model)
 def write_rephrased_questions(rephrased_questions, model):
     for c, questions in rephrased_questions.items():
         if questions:
-            path = f"datasets/refusal/{model}_{c}_rephrased_questions.jsonl"
+            path = f"datasets/refusal/{model}_{c}_rephrased_jailbreaks.jsonl"
             with jsonlines.open(path, mode='w') as writer:
                 for question in questions:
                     writer.write(question)
@@ -179,19 +203,32 @@ def main():
                         help='Enable rephrasing of generated questions')
     parser.add_argument('--model', type=str, default="gpt-3.5-turbo-16k-0613",
                         help='Model to use for generation and rephrasing')
+    parser.add_argument('--mode', type=str, default="concept")
     args = parser.parse_args()
 
     random.seed(64)
-    file_path = 'datasets/refusal/example_concepts.jsonl'
+    if args.mode == "concept":
+        file_path = 'datasets/refusal/example_concepts.jsonl'
+    elif args.mode == "prompt":
+        file_path = 'datasets/refusal/example_jailbreaks.jsonl'
+    else:
+        print("Invalid mode selected. Please choose either 'prompt' or 'concept'.")
+        return
 
     documents = load_documents(file_path)
 
-    categories = criminal_concepts + bad_concepts + neutral_concepts
+    if args.mode == "concept":
+        categories = criminal_concepts  # + bad_concepts + neutral_concepts
+    elif args.mode == "prompt":
+        categories = ["advice_column_template", "email_reply_framework",
+                      "interview_questions_set", "debate_motion_proposer"]
+        #    "newspaper_advice_column", "expert_insight", "deep_dive_exploration", "critical_analysis", "concept_breakdown", "future_vision", "problem_solving_guide", "in-depth_review", "life_hack", "creative_thinking", "innovative_solution", "story_based_explanation", "cross_disciplinary_analysis", "advice_column", "theory_application",
+        #             "skill_building_tips", "knowledge_synthesis", "comparative_study", "practical_guidance", "strategic_advice", "theoretical_exploration", "detailed_instruction", "speculative_scenario", "case_study_analysis", "informed_opinion", "global_perspective", "ethical_consideration", "cultural_insight", "tech_innovation_discussion", "sustainability_focus"]
     generated_questions = load_generated_questions(categories, args.model)
 
     for c in categories:
         generated_questions = generate_questions_for_category(
-            c, args.model, documents, SAFE_MODELS, generated_questions)
+            c, args.model, documents, SAFE_MODELS, generated_questions, args.mode)
         write_generated_questions(generated_questions, args.model)
 
         if args.rephrase:
@@ -202,7 +239,7 @@ def main():
                 continue
             print(f"Rephrasing questions for category {c}")
             rephrased_questions = rephrase_category(
-                generated_questions, rephrased_questions, c, args.model)
+                generated_questions, rephrased_questions, c, args.model, args.mode)
             print(rephrased_questions)
             write_rephrased_questions(rephrased_questions, args.model)
 
